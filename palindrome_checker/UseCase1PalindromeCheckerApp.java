2095d559afcd8577f9f83b605dd99fab3f127389
MRvandals4vage <iu9736@srmist.edu.in>
2026-02-26 09:39:59 +0530

    UC1 commit welcome message

---
+++ palindrome_checker/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class UseCase1PalindromeCheckerApp.java {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the palindrome checker app made in java\n");
+        System.out.println("Version: 1.0\n");
+        System.out.println("System initialized successfully\n");
+    }
+}
