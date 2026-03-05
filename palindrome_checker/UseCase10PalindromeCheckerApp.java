98bb12dbae6dfddfe8df16ef0f3df75f67726b45
unknown <rayirthmisra25@gmail.com>
2026-03-05 22:36:34 +0530

    commit for UC10

---
+++ palindrome_checker/UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class NormalizedPalindrome {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String input = sc.nextLine();
+
+        // Normalize string
+        input = input.replaceAll("\\s+", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        int left = 0;
+        int right = input.length() - 1;
+
+        // Compare characters
+        while (left < right) {
+
+            if (input.charAt(left) != input.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
+        }
+
+        // Output result
+        if (isPalindrome) {
+            System.out.println("Palindrome (ignoring spaces and case)");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+
+        sc.close();
+    }
+}
